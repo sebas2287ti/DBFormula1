f7532a5ddda0b991af19d0ed209ece5bbfd55c87
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-01 22:17:55 -0500

    Planteamiento la estructura de carga para la interfaz grafica mediante el uso de JavaSwing el cual se inicializa en DBFormula1 y la matematica de manejo de los paneles mediante un InterfaceController basado en un cardlayout

---
+++ src/com/DBFormula1/DBFormula1.java
@@ -0,0 +1,22 @@
+package com.DBFormula1;
+
+import com.DBFormula1.infrastructure.ui.components.InterfaceController;
+import javax.swing.*;
+
+public class DBFormula1 {
+    public static void main (String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            //Creacion de la ventana JFrame
+            JFrame windowMain = new JFrame("F1 Manager");
+            windowMain.setSize(1280, 720);
+            windowMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            windowMain.setLocationRelativeTo(null);
+            windowMain.setResizable(false);
+
+            //Iniciar el controlador de todos los paneles dentro de la ventana principal y volverla visible
+            InterfaceController controller = new InterfaceController(windowMain);
+            windowMain.setVisible(true);
+        });
+    }
+}
---
+++ src/com/DBFormula1/infrastructure/ui/components/InterfaceController.java
@@ -0,0 +1,34 @@
+package com.DBFormula1.infrastructure.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InterfaceController {
+    private final CardLayout CARDLAYOUT;
+    private final JPanel CONTAINER;
+
+    //Creacion del controlador de la interfaz
+    public InterfaceController(JFrame windowMain) {
+        this.CARDLAYOUT = new CardLayout();
+        this.CONTAINER = new JPanel(CARDLAYOUT);
+
+        //Inicializacion de los paneles con este controlador
+
+        // Creacion del objecto panel example  -- StartPanel startPanel =  new StartPanel(this);
+
+
+        //Agregar los paneles inicializados al panel container el cual los contendra a todos
+
+        // Se agregan al contenedor del cardlayoud para su carga y almacenaje -- CONTAINER.add(startPanel, "StartPanel");
+
+
+        //Agregar el panel contenedor a la ventana principal donde se mostrara
+        windowMain.add(CONTAINER);
+        windowMain.setVisible(true);
+    }
+
+    //Metodo que permite cambiar entre paneles visibles y no visibles en la ventana Principal
+    public void panelChange(String panelName) {
+        CARDLAYOUT.show(CONTAINER, panelName);
+    }
+}
