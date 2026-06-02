f9463d8a425ba1a021a464f0d8debb8e99dd490b
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-01 22:20:12 -0500

    Creacion de componente para el dibujo de imagenes de fondo en los Jpanel mediante la clase Background.java la cual se encarga de obtener la imagen mediante la direccion y pintarla en el panel

---
+++ src/com/DBFormula1/infrastructure/ui/components/Background.java
@@ -0,0 +1,36 @@
+package com.DBFormula1.infrastructure.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+
+public class Background extends JPanel {
+    private Image imageBackground;
+
+    //Obtencion de la imagen apartir de la ubicacion en sources
+    public Background(String locationImage) {
+
+        URL urlLocationImage = getClass().getResource(locationImage);
+
+        if (urlLocationImage != null) {
+            this.imageBackground = new ImageIcon(urlLocationImage).getImage();
+        }
+
+        setOpaque(false);
+    }
+
+    //Encargado de pintar la imagen
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (imageBackground != null) {
+            g.drawImage(imageBackground, 0, 0, getWidth(), getHeight(), this);
+        } else {
+
+            g.setColor(new Color(22, 27, 46));
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
+    }
+}
