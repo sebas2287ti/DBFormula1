59fdb0d7eaa0463dfc24b5d8ba0692fc14b3a9b5
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-02 10:34:33 -0500

    Se agrega el Uifactory para la creacion de botones simples

---
+++ src/com/DBFormula1/infrastructure/ui/components/UiFactory.java
@@ -0,0 +1,36 @@
+package com.DBFormula1.infrastructure.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UiFactory {
+
+    //Metodo de creacion de botones apartir de (Icono, Cordenadas x y, Ancho, Alto, Color del fondo, Color del Icono, Color de la margen, Color del icono al pasar encima de el)
+    public static JButton createButton(String symbol, int x, int y, int w, int h, Color background, Color text, Color border, Color reColorText) {
+        JButton button = new JButton(symbol);
+
+        //Definicion de las caracteristicas del boton (posicion, color de fondo, color de borde)
+        button.setBounds(x, y, w, h);
+        button.setBackground(background);
+        button.setFont(new Font("SansSerif", Font.BOLD, 24));
+        button.setForeground(text);
+        button.setFocusPainted(false);
+        button.setBorder(BorderFactory.createLineBorder(border, 1));
+
+        //Creacion de eventos cuando el mouse esta encima del boton y cuando no
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                button.setBackground(border);
+                button.setForeground(reColorText);
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                button.setBackground(background);
+                button.setForeground(text);
+            }
+        });
+
+        //retorno del boton totalmente creado para el uso
+        return button;
+    }
+}
