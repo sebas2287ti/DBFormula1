20004e9e5c012edf92a52a312b80d9328f509f0e
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-01 22:11:02 -0500

    Planteamiento de la estructura del proyecto y se agrega el modulo DataConnection para las conexiones con la Base de Datos

---
+++ src/com/DBFormula1/infrastructure/persistence/DataConnection.java
@@ -0,0 +1,18 @@
+package com.DBFormula1.infrastructure.persistence;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class DataConnection {
+    //Credenciales para la conexion con la base de datos (guardadas en el .env)
+    private static final String KEY = System.getenv("DB_PASSWORD");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String URL = System.getenv("DB_URL");
+
+
+    //Metodo para realizar la conexion a la base de datos
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, KEY);
+    }
+}
