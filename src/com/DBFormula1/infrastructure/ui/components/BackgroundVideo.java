1e370ddcd0e2359c739328b7745e4091b98756c3
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-03 00:32:42 -0500

    se añaden nuevas funciones para el cambio de los paneles

--- src/com/DBFormula1/infrastructure/ui/components/BackgroundVideo.java
+++ src/com/DBFormula1/infrastructure/ui/components/BackgroundVideo.java
@@ -2,6 +2,7 @@ package com.DBFormula1.infrastructure.ui.components;
 
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 
@@ -12,6 +13,18 @@ public class BackgroundVideo {
     //Construtor el cual definite que va a ser un nuevo componente de video
     public BackgroundVideo() {
         this.mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        this.mediaPlayerComponent.setVisible(false);
+
+        this.mediaPlayerComponent.mediaPlayer().events().addMediaPlayerEventListener(
+                new uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter() {
+                    @Override
+                    public void finished(uk.co.caprica.vlcj.player.base.MediaPlayer mediaPlayer) {
+                        SwingUtilities.invokeLater(() -> {
+                            mediaPlayerComponent.setVisible(false);
+                        });
+                    }
+                }
+        );
     }
 
     //Retorna para la vizualizacion el componente el cual sera el que tendra el video dentro
@@ -26,6 +39,11 @@ public class BackgroundVideo {
 
         //Si el archivo si existe crea el video apartir de la url que proporcionamos
         if (video.exists()) {
+
+            if (!mediaPlayerComponent.isVisible()) {
+                mediaPlayerComponent.setVisible(true);
+            }
+
             mediaPlayerComponent.mediaPlayer().media().play(URL);
         } else {
             System.out.print("Error video");
@@ -35,5 +53,17 @@ public class BackgroundVideo {
     //Cuando se cierra java 25 se necesita eliminar todos los datos que queden en el hilo para eso el KillResources
     public void killResources() {
         mediaPlayerComponent.release();
+    }
+
+    public void pauseVideo() {
+        if (mediaPlayerComponent != null && mediaPlayerComponent.mediaPlayer().status().isPlaying()) {
+            mediaPlayerComponent.mediaPlayer().controls().pause();
+        }
+    }
+
+    public void resumeVideo() {
+        if (mediaPlayerComponent != null && !mediaPlayerComponent.mediaPlayer().status().isPlaying()) {
+            mediaPlayerComponent.mediaPlayer().controls().play();
+        }
     }
 }
