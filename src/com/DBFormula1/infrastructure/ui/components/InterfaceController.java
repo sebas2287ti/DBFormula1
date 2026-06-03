5957bef9795f72eea2874bdc7c1d4674caa76c73
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-03 00:33:39 -0500

    Se modifica la class InterfaceController para el soporte del nuevo panel que es el inicial

--- src/com/DBFormula1/infrastructure/ui/components/InterfaceController.java
+++ src/com/DBFormula1/infrastructure/ui/components/InterfaceController.java
@@ -1,6 +1,7 @@
 package com.DBFormula1.infrastructure.ui.components;
 
 import com.DBFormula1.infrastructure.ui.mainWindow.TeamsFormula1;
+import com.DBFormula1.infrastructure.ui.mainWindow.StartFormula1;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,10 +19,12 @@ public class InterfaceController {
 
         // Creacion del objecto panel example  -- StartPanel startPanel =  new StartPanel(this);
         TeamsFormula1 teamsFormula1 = new TeamsFormula1(this);
+        StartFormula1 startFormula1 = new StartFormula1(this);
 
         //Agregar los paneles inicializados al panel container el cual los contendra a todos
 
         // Se agregan al contenedor del cardlayoud para su carga y almacenaje -- CONTAINER.add(startPanel, "StartPanel");
+        CONTAINER.add(startFormula1, "StartPanel");
         CONTAINER.add(teamsFormula1, "TeamsPanel");
 
         //Agregar el panel contenedor a la ventana principal donde se mostrara
