0e1358f8d92c0ec14c1138330b5fd65c2578c3dc
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-03 00:34:29 -0500

    Se agrega la class PilotF1 para el soporte de BD

---
+++ src/com/DBFormula1/domain/entities/PilotF1.java
@@ -0,0 +1,131 @@
+package com.DBFormula1.domain.entities;
+
+//Definicion de la clases piloto y sus caracteritsticas
+public class PilotF1 {
+    private Integer id;
+    private Integer año;
+    private String equipo;
+    private String nombre;
+    private Integer n_Piloto;
+    private Boolean pilotoPrincipal;
+    private Boolean ganadorMundial;
+    private Integer posicionCampeonato  ;
+    private Integer puntosCampeonato;
+    private Integer victorias;
+    private Integer poles;
+    private Integer N_campeonatos;
+    private Integer total_Carreras;
+
+    //Constructor completo
+    public PilotF1(Integer id, Integer año, String equipo, String nombre, Integer n_Piloto, Boolean pilotoPrincipal, Boolean ganadorMundial, Integer posicionCampeonato, Integer puntosCampeonato, Integer victorias, Integer poles, Integer N_campeonatos, Integer total_Carreras) {
+        this.id = id;
+        this.año = año;
+        this.equipo = equipo;
+        this.nombre = nombre;
+        this.n_Piloto = n_Piloto;
+        this.pilotoPrincipal = pilotoPrincipal;
+        this.ganadorMundial = ganadorMundial;
+        this.posicionCampeonato = posicionCampeonato;
+        this.puntosCampeonato = puntosCampeonato;
+        this.victorias = victorias;
+        this.poles = poles;
+        this.N_campeonatos = N_campeonatos;
+        this.total_Carreras = total_Carreras;
+    }
+
+    //constructor vacio
+    public PilotF1() {
+    }
+
+    //Metodos get
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAño() {
+        return año;
+    }
+    public void setAño(Integer año) {
+        this.año = año;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getN_Piloto() {
+        return n_Piloto;
+    }
+    public void setN_Piloto(Integer n_Piloto) {
+        this.n_Piloto = n_Piloto;
+    }
+
+    public Boolean getPilotoPrincipal() {
+        return pilotoPrincipal;
+    }
+    public void setPilotoPrincipal(Boolean pilotoPrincipal) {
+        this.pilotoPrincipal = pilotoPrincipal;
+    }
+
+    public Boolean getGanadorMundial() {
+        return ganadorMundial;
+    }
+    public void setGanadorMundial(Boolean ganadorMundial) {
+        this.ganadorMundial = ganadorMundial;
+    }
+
+    public Integer getPosicionCampeonato() {
+        return posicionCampeonato;
+    }
+    public void setPosicionCampeonato(Integer posicionCampeonato) {
+        this.posicionCampeonato = posicionCampeonato;
+    }
+
+    public Integer getPuntosCampeonato() {
+        return puntosCampeonato;
+    }
+    public void setPuntosCampeonato(Integer puntosCampeonato) {
+        this.puntosCampeonato = puntosCampeonato;
+    }
+
+    public Integer getVictorias() {
+        return victorias;
+    }
+    public void setVictorias(Integer victorias) {
+        this.victorias = victorias;
+    }
+
+    public Integer getPoles() {
+        return poles;
+    }
+    public void setPoles(Integer poles) {
+        this.poles = poles;
+    }
+
+    public Integer getN_campeonatos() {
+        return N_campeonatos;
+    }
+    public void setN_campeonatos(Integer N_campeonatos) {
+        this.N_campeonatos = N_campeonatos;
+    }
+
+    public Integer getTotal_Carreras() {
+        return total_Carreras;
+    }
+    public void setTotal_Carreras(Integer total_Carreras) {
+        this.total_Carreras = total_Carreras;
+    }
+}
