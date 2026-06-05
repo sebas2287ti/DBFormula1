82f4746c8dfb73f079864274107aae8a56f06111
sebas2287ti <Sebas2287ti@gmail.com>
2026-06-05 10:29:12 -0500

    Se modifica el InterfaceController para agregar el soporte de cambio de pantalla a FindPanel Y FormPanel

--- src/com/DBFormula1/infrastructure/ui/components/InterfaceController.java
+++ src/com/DBFormula1/infrastructure/ui/components/InterfaceController.java
@@ -2,6 +2,8 @@ package com.DBFormula1.infrastructure.ui.components;
 
 import com.DBFormula1.infrastructure.ui.mainWindow.TeamsFormula1;
 import com.DBFormula1.infrastructure.ui.mainWindow.StartFormula1;
+import com.DBFormula1.infrastructure.ui.mainWindow.FindFormula1;
+import com.DBFormula1.infrastructure.ui.mainWindow.FormFormula1;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,12 +22,17 @@ public class InterfaceController {
         // Creacion del objecto panel example  -- StartPanel startPanel =  new StartPanel(this);
         TeamsFormula1 teamsFormula1 = new TeamsFormula1(this);
         StartFormula1 startFormula1 = new StartFormula1(this);
+        FindFormula1 findFormula1 = new FindFormula1(this);
+        FormFormula1 formFormula1 = new FormFormula1(this);
+
 
         //Agregar los paneles inicializados al panel container el cual los contendra a todos
 
         // Se agregan al contenedor del cardlayoud para su carga y almacenaje -- CONTAINER.add(startPanel, "StartPanel");
         CONTAINER.add(startFormula1, "StartPanel");
         CONTAINER.add(teamsFormula1, "TeamsPanel");
+        CONTAINER.add(findFormula1, "FindPanel");
+        CONTAINER.add(formFormula1, "FormPanel");
 
         //Agregar el panel contenedor a la ventana principal donde se mostrara
         windowMain.add(CONTAINER);
